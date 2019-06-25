cf64d8933e2cc01c44e84ad5928259a9361797b7
Kristaps Austers <kristaps@austers.lv>
2019-06-25 23:04:36 +0300

    Calculate & write age using JS

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Already running deferred so no need for DOMContentLoaded
+
+/**
+ * @returns {number}
+ */
+function getAge() {
+	const birth = 719141700 * 1000;
+	const now = Date.now();
+	const age = new Date(now - birth);
+	return age.getUTCFullYear() - 1970;
+}
+
+/**
+ * @returns {Element}
+ */
+function getTarget() {
+	return document.getElementsByTagName('output')[0];
+}
+
+getTarget().innerText = getAge();
