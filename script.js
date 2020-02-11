043ee148d159d5007a37050d2a62023805ff5a56
Kristaps Austers <kristaps@austers.lv>
2020-02-11 21:17:43 +0200

    Fix age calculation

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getAge() {
 	const birth = 719141700 * 1000;
 	const now = Date.now();
 	const age = new Date(now - birth);
-	return age.getUTCFullYear() - 1970;
+	return age.getFullYear() - 1970;
 }
 
 /**
