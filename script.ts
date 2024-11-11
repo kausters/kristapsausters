c9051317cad805a6cee7526d5093fbd69e72bfa3
Kristaps <kristaps@austers.lv>
2024-11-11 23:50:28 +0200

    Fade in list items in index

--- script.ts
+++ script.ts
@@ -1,14 +1,23 @@
 'use strict';
 
-function getAge() {
+function setAge() {
 	const birth = 719141700 * 1000;
 	const now = Date.now();
-	const age = new Date(now - birth);
 
 	// JS isn't great with dates that are a duration
-	return age.getFullYear() - 1970;
+	const age = new Date(now - birth).getFullYear() - 1970;
+
+	const element = document.getElementById('age');
+	element.innerText = age.toString();
+}
+
+function setItemIndex() {
+	const list = document.querySelector('ul');
+	Array.from(list.children).forEach((item: HTMLElement, index) => {
+		item.style.setProperty('--index', (index + 1).toString());
+	});
 }
 
 // Already running deferred so no need for DOMContentLoaded
-const element = document.getElementById('age');
-element.innerText = getAge().toString();
+setAge();
+setItemIndex();
